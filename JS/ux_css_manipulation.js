9bdf27eac7b34aae60396508bf9e6b0a9e7bfd4d
eduardadu <40005532+eduardadu@users.noreply.github.com>
2020-03-29 15:51:33 +0100

    Toma la de volta

--- JS/ux_css_manipulation.js
+++ JS/ux_css_manipulation.js
@@ -2,12 +2,28 @@
 $( document ).ready(function() {
   var seta = document.querySelector('#setafora');
   var bar = document.querySelectorAll('.sidebar');
+  var slideOut=false;
+  //  var slideIn=false;
 
+
+//---------------------------------------------------------------SIDEBAR IN AND OUT
   seta.addEventListener("click", function(){
-    for(var i=0; i<bar.length;i++){
-      bar[i].style.left="auto";
-      bar[i].style.right="0px";
-      //bar[i].addClass('aniSlidebar');
+    var width = document.body.clientWidth;
+    if(slideOut==false){
+      for(var i=0; i<bar.length;i++){
+        bar[i].style.left="auto";
+        bar[i].style.right="0px";
+        bar[i].classList.add('aniSlidebar');
+      }
+    }else{
+      for(var i=0; i<bar.length;i++){
+        bar[i].style.left='"' + width + '"';
+        bar[i].style.right="auto";
+      }
     }
+    slideOut=!slideOut;
   });
+
+//----------------------------------------------------------------------------------
+
 });
