09813b9d51e150cd4e16cce59593364d17a1d538
eduardadu <40005532+eduardadu@users.noreply.github.com>
2020-03-29 19:01:00 +0100

    G

--- JS/ux_css_manipulation.js
+++ JS/ux_css_manipulation.js
@@ -2,6 +2,7 @@
 $( document ).ready(function() {
   var seta = document.querySelector('#setafora');
   var bar = document.querySelectorAll('.sidebar');
+  var barUp = document.querySelectorAll('.centerbar');
   var slideOut=false;
   //  var slideIn=false;
 
@@ -16,17 +17,20 @@ $( document ).ready(function() {
       //  bar[i].style.right="0%";
         bar[i].classList.add('move');
         bar[i].style.minWidth=  "200px";
-        document.querySelector('#search_container').style.marginRight=  "2 rem";
-        document.querySelector('#setafora').style.transform = 'rotate(180deg)';
       }
+      barUp[0].classList.add('moveUP');
+    //  document.querySelector('#search_container').style.marginRight=  "2 rem";
+      document.querySelector('#setafora').style.transform = 'rotate(180deg)';
+
     }else{
+      //  document.querySelector('#search_container').style.marginRight=  "0 rem";
       for(var i=0; i<bar.length;i++){
-            document.querySelector('#search_container').style.marginRight=  "0 rem";
             bar[i].classList.remove('move');
             bar[i].style.minWidth=  "0px";
-            document.querySelector('#setafora').style.transform = 'rotate(0deg)';
 
       }
+      barUp[0].classList.remove('moveUP');
+      document.querySelector('#setafora').style.transform = 'rotate(0deg)';
     }
     slideOut=!slideOut;
   });
